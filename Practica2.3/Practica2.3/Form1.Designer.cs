b0b505f8a2aea07497d1095593b712ac05aa8266
JOSEPH ANDRE ARIZPE COLMENARES <josaricol@alu.edu.gva.es>
2022-11-26 13:17:21 +0100

    codigo del cobro

--- Practica2.3/Practica2.3/Form1.Designer.cs
+++ Practica2.3/Practica2.3/Form1.Designer.cs
@@ -32,7 +32,7 @@
             this.txt1 = new System.Windows.Forms.TextBox();
             this.txt2 = new System.Windows.Forms.TextBox();
             this.lbl2 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.chb1 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -69,15 +69,15 @@
             this.lbl2.TabIndex = 3;
             this.lbl2.Text = "Coste:";
             // 
-            // checkBox1
+            // chb1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(63, 286);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(70, 17);
-            this.checkBox1.TabIndex = 4;
-            this.checkBox1.Text = "Urgente?";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.chb1.AutoSize = true;
+            this.chb1.Location = new System.Drawing.Point(63, 286);
+            this.chb1.Name = "chb1";
+            this.chb1.Size = new System.Drawing.Size(70, 17);
+            this.chb1.TabIndex = 4;
+            this.chb1.Text = "Urgente?";
+            this.chb1.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
@@ -87,6 +87,7 @@
             this.button1.TabIndex = 5;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -94,7 +95,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(537, 450);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.chb1);
             this.Controls.Add(this.lbl2);
             this.Controls.Add(this.txt2);
             this.Controls.Add(this.txt1);
@@ -112,7 +113,7 @@
         private System.Windows.Forms.TextBox txt1;
         private System.Windows.Forms.TextBox txt2;
         private System.Windows.Forms.Label lbl2;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox chb1;
         private System.Windows.Forms.Button button1;
     }
 }
